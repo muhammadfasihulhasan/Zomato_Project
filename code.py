a3f897a7c3ee1faaa3f1005b368628194e5af11c
muhammadfasihulhasan <155235452+muhammadfasihulhasan@users.noreply.github.com>
2024-02-12 01:17:02 +0100

    Uploading dataset

--- code.py
+++ code.py
@@ -4,3 +4,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
+st.title("Zomato Bangalore Restaurants")
+st.subheader("About the Project")
+st.write("This project focuses on")
+
+df = pd.read_csv('zomato.csv')
+
